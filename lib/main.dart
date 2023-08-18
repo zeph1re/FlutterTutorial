c3c19087a4785b07fb5b4135ee377e05501e45fc
zeph1re <arlinnog1122@gmail.com>
2023-08-18 22:15:32 +0700

    add stack and alignment

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,46 +12,104 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  Random random = Random();
+  String dummy =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tellus velit, fringilla non dapibus non, blandit pulvinar lectus. Sed feugiat vel enim vitae euismod. Proin ac velit non velit convallis rutrum sit amet et enim. Nam dictum erat in fermentum mollis. Vivamus vitae urna hendrerit, sodales tellus in, imperdiet mi. Aliquam pretium ornare libero eu volutpat. Maecenas varius erat mauris, non faucibus tellus mattis a.";
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(
-              title: const Text("Flexible Layout"),
+              title: const Text("Stack and Align"),
             ),
-            body: Column(
+            body: Stack(
               children: [
-                Flexible(
-                  flex: 1,
-                  child: Row(children: [
+                // Background
+                Column(
+                  children: [
                     Flexible(
                         flex: 1,
-                        child: Container(
-                            margin: const EdgeInsets.all(10),
-                            color: Colors.green)),
+                        child: Row(
+                          children: [
+                            Flexible(
+                              flex: 1,
+                              child: Container(
+                                color: Colors.amberAccent,
+                              ),
+                            ),
+                            Flexible(
+                              flex: 1,
+                              child: Container(
+                                color: Colors.green,
+                              ),
+                            )
+                          ],
+                        )),
                     Flexible(
                         flex: 1,
-                        child: Container(
-                            margin: const EdgeInsets.all(10),
-                            color: Colors.amber)),
-                    Flexible(
-                        flex: 1,
-                        child: Container(
-                            margin: const EdgeInsets.all(10),
-                            color: Colors.red)),
-                  ]),
+                        child: Row(
+                          children: [
+                            Flexible(
+                              flex: 1,
+                              child: Container(
+                                color: Colors.amber,
+                              ),
+                            ),
+                            Flexible(
+                              flex: 1,
+                              child: Container(
+                                color: Colors.blue,
+                              ),
+                            )
+                          ],
+                        ))
+                  ],
                 ),
-                Flexible(
-                    flex: 2,
-                    child: Container(
-                        margin: const EdgeInsets.all(10), color: Colors.blue)),
-                Flexible(
-                    flex: 1,
-                    child: Container(
-                        margin: const EdgeInsets.all(10),
-                        color: Colors.greenAccent)),
+                // listview
+
+                ListView(
+                  children: [
+                    Column(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.all(10),
+                          child: Text(
+                            dummy,
+                            style: const TextStyle(fontSize: 30),
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.all(10),
+                          child: Text(
+                            dummy,
+                            style: const TextStyle(fontSize: 30),
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.all(10),
+                          child: Text(
+                            dummy,
+                            style: const TextStyle(fontSize: 30),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                // button tengah bawah
+                Align(
+                  alignment: const Alignment(0, 0.90),
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.green,
+                      ),
+                      child: const Text(
+                        "Button",
+                        style: TextStyle(fontSize: 16),
+                      )),
+                )
               ],
             )));
   }
