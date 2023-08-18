ab075c0e851352a8b0eb0f00d1f6625ecfefd952
zeph1re <arlinnog1122@gmail.com>
2023-08-18 23:30:18 +0700

    add image widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,113 +4,29 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String dummy =
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tellus velit, fringilla non dapibus non, blandit pulvinar lectus. Sed feugiat vel enim vitae euismod. Proin ac velit non velit convallis rutrum sit amet et enim. Nam dictum erat in fermentum mollis. Vivamus vitae urna hendrerit, sodales tellus in, imperdiet mi. Aliquam pretium ornare libero eu volutpat. Maecenas varius erat mauris, non faucibus tellus mattis a.";
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(
-              title: const Text("Stack and Align"),
-            ),
-            body: Stack(
-              children: [
-                // Background
-                Column(
-                  children: [
-                    Flexible(
-                        flex: 1,
-                        child: Row(
-                          children: [
-                            Flexible(
-                              flex: 1,
-                              child: Container(
-                                color: Colors.amberAccent,
-                              ),
-                            ),
-                            Flexible(
-                              flex: 1,
-                              child: Container(
-                                color: Colors.green,
-                              ),
-                            )
-                          ],
-                        )),
-                    Flexible(
-                        flex: 1,
-                        child: Row(
-                          children: [
-                            Flexible(
-                              flex: 1,
-                              child: Container(
-                                color: Colors.amber,
-                              ),
-                            ),
-                            Flexible(
-                              flex: 1,
-                              child: Container(
-                                color: Colors.blue,
-                              ),
-                            )
-                          ],
-                        ))
-                  ],
-                ),
-                // listview
-
-                ListView(
-                  children: [
-                    Column(
-                      children: [
-                        Container(
-                          margin: const EdgeInsets.all(10),
-                          child: Text(
-                            dummy,
-                            style: const TextStyle(fontSize: 30),
-                          ),
-                        ),
-                        Container(
-                          margin: const EdgeInsets.all(10),
-                          child: Text(
-                            dummy,
-                            style: const TextStyle(fontSize: 30),
-                          ),
-                        ),
-                        Container(
-                          margin: const EdgeInsets.all(10),
-                          child: Text(
-                            dummy,
-                            style: const TextStyle(fontSize: 30),
-                          ),
-                        )
-                      ],
-                    )
-                  ],
-                ),
-                // button tengah bawah
-                Align(
-                  alignment: const Alignment(0, 0.90),
-                  child: ElevatedButton(
-                      onPressed: () {},
-                      style: TextButton.styleFrom(
-                        foregroundColor: Colors.white,
-                        backgroundColor: Colors.green,
-                      ),
-                      child: const Text(
-                        "Button",
-                        style: TextStyle(fontSize: 16),
-                      )),
-                )
-              ],
-            )));
+      appBar: AppBar(
+        title: const Text("Image Widget"),
+      ),
+      body: Center(
+        child: Container(
+          color: Colors.amber,
+          width: 200,
+          height: 200,
+          padding: const EdgeInsets.all(1),
+          child: const Image(
+            image: AssetImage("images/arlinnoganda.jpg"),
+            fit: BoxFit.contain,
+            repeat: ImageRepeat.repeat,
+          ),
+        ),
+      ),
+    ));
   }
 }
